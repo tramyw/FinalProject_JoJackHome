983f454a4585db5c970d6d17f941e1b5e41b2149
Tramy <tramy.w@gmail.com>
2024-01-03 11:01:45 +0800

    removed unnecessary file, updated package-lock.json & package.json & index.js, added banner component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import AdoptionInfoPage from "./AdoptionInfoPage";
 import Navbar from "./Navbar";
+import Profile from "./Profile";
+import Banner from "./components/Banner";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    {/* <Navbar /> */}
+    <Banner />
     <AdoptionInfoPage />
+    <Profile />
   </React.StrictMode>
 );
