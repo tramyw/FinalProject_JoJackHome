13baa5c663e44ac9d44ff3df2d0db2f0b8970078
Tramy <tramy.w@gmail.com>
2024-01-03 11:10:43 +0800

    showed Navbar in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Banner from "./components/Banner";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Navbar /> */}
+    <Navbar />
     <Banner />
     <AdoptionInfoPage />
     <Profile />
