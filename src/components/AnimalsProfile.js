9f4318c10b559d0c39e5e1718ce8383fc6e59e0c
chiukiu <ckiu@outlook.com>
2024-01-09 12:24:35 +0800

    JC commit

--- src/components/AnimalsProfile.js
+++ src/components/AnimalsProfile.js
@@ -1,11 +1,10 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import HKSCDA_info from "../HKSCDA_animal_info.json";
 import { Button, Stack } from "@mui/material";
 import Navbar from "../Navbar";
 import Footer from "./Footer";
-import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
 import ImageG from "./ImageGallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 import "../Profile.css";
 
 function AnimalsProfile() {
@@ -17,60 +16,53 @@ function AnimalsProfile() {
   let animalInfo = unfilteredInfo.filter((el) => el.name === animal_Name);
   return (
     <div>
-      <div className="containerA">
-        <Navbar />
-        <Stack className="content">
-          <div>
-            <ImageG data={animalInfo} />
-          </div>
-          <div>
-            <h1>{animalInfo[0].name}</h1>
-            <p>
-              我係{animalInfo[0].gender}仔 {animalInfo[0].specis}, 我的歲數是{" "}
-              {animalInfo[0].age}
-            </p>
-            <p>
-              我現時
-              {
-                <em>
-                  <span style={{ textDecoration: "underline" }}>
-                    {animalInfo[0].chipped}
-                  </span>
-                </em>
-              }
-              晶片, 而且
+      <Navbar />
+      <div className="content">
+        <ImageG data={animalInfo} />
+        <div>
+          <h1>{animalInfo[0].name}</h1>
+          <p>
+            我係{animalInfo[0].gender}仔 {animalInfo[0].specis}, 我的歲數是{" "}
+            {animalInfo[0].age}
+          </p>
+          <p>
+            我現時
+            {
               <em>
-                <span style={{ textDecoration: "underline" }}>
-                  {animalInfo[0].castrated}
-                </span>
+                <span className="underline">{animalInfo[0].chipped}</span>
               </em>
-              ~
-            </p>
-            <br></br>
-            <br></br>
-            <p>
-              性別: {animalInfo[0].gender}({animalInfo[0].castrated})
-            </p>
-            <p>歲數: {animalInfo[0].age}</p>
-            <p>晶片: {animalInfo[0].chipped}</p>
-            <br></br>
-            <br></br>
-            <div>
-              <p>狗狗準備好返屋企，你準備好未?</p>
-              <h3>領養前，停一停，諗一諗</h3>
-            </div>
-            <Stack spacing={2}>
-              <a href={`${animalInfo[0].href}`}>
-                <Button variant="contained">去了解我更多</Button>
-              </a>
-            </Stack>
+            }
+            晶片, 而且
+            <em>
+              <span className="underline">{animalInfo[0].castrated}</span>
+            </em>
+            ~
+          </p>
+          <br></br>
+          <br></br>
+          <p>
+            性別: {animalInfo[0].gender}({animalInfo[0].castrated})
+          </p>
+          <p>歲數: {animalInfo[0].age}</p>
+          <p>晶片: {animalInfo[0].chipped}</p>
+          <br></br>
+          <br></br>
+          <div>
+            <p>狗狗準備好返屋企，你準備好未?</p>
+            <h3>領養前，停一停，諗一諗</h3>
           </div>
-          <div></div>
-        </Stack>
-        <br></br>
-        <br></br>
+          <Stack spacing={2}>
+            <a href={`${animalInfo[0].href}`}>
+              <Button variant="contained">去了解我更多</Button>
+            </a>
+          </Stack>
+        </div>
       </div>
-
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <Footer />
     </div>
   );
